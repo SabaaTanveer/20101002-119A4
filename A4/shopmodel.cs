b099d20d39d9275dcc02a5a0632961b4a240d9e9
SabaaTanveer <149601503+SabaaTanveer@users.noreply.github.com>
2024-01-17 12:56:46 -0800

    Add files via upload

---
+++ A4/shopmodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace my_wayshop.Models
+{
+    public class shopmodel
+
+    {
+        public List<M_Category> cat { get; set; }
+        public List<M_Product> prod { get; set; }
+
+    }
+}
